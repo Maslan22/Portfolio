db8bc72258283ceb6cff7d0cc303a00b4cee8973
Maslan Iddrisu <henrycoffie22@gmail.com>
2024-07-06 20:56:24 +0000

    hero

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -32,7 +32,7 @@ export async function GET() {
 
   return NextResponse.redirect(
     process.env.NODE_ENV === "development"
-      ? "http://localhost:3000/"
+      ? "http://localhost:3000"
       : "https://portfolio-two-gray-49.vercel.app/guestbook"
   );
 }
